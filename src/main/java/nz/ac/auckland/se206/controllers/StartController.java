eaa5ce6fdeb5ba69429e7e3f12bec0b310d56d81
0x20000 <zhe570@aucklanduni.ac.nz>
2023-09-13 05:04:15 +1200

    feat:start screen now functional

--- src/main/java/nz/ac/auckland/se206/controllers/StartController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartController.java
@@ -1,5 +1,30 @@
 package nz.ac.auckland.se206.controllers;
 
-public class StartController {
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import nz.ac.auckland.se206.App;
 
+public class StartController {
+@FXML
+  private Button startButton;
+@FXML
+  private Button creditButton;
+@FXML
+  private Button exitButton;
+  @FXML
+  private void start() {
+    try {
+      App.setRoot("level");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @FXML
+  private void credit() {
+  }
+  @FXML
+  private void exit() {
+    System.exit(0);
+  }
 }
