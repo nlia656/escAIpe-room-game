b031c3b391326b9f9bfe3d1f99e6a7b56ded1c7f
0x20000 <zhe570@aucklanduni.ac.nz>
2023-09-18 10:44:02 +1200

    feat: change how answer get from GameState

---
+++ src/main/java/nz/ac/auckland/se206/controllers/ArtroomController.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+
+public class ArtroomController {
+
+    @FXML
+    private void initialize() {
+    }
+}
