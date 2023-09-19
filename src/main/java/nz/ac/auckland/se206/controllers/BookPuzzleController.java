746e482ba3f81318518f73f891b7814c162e07e8
nlia656 <nlia656@aucklanduni.ac.nz>
2023-09-19 14:05:34 +1200

    feat: made certain snippets visible depending on the answer selected for the particular game

--- src/main/java/nz/ac/auckland/se206/controllers/BookPuzzleController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/BookPuzzleController.java
@@ -1,7 +1,28 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class BookPuzzleController {
   public static String puzzleAnswer;
+  @FXML private ImageView couch1Snip;
+  @FXML private ImageView dinosaurSnip;
+  @FXML private ImageView maskSnip;
+  @FXML private ImageView robeSnip;
+  @FXML private ImageView poster3Snip;
+  @FXML private ImageView poster2Snip;
+  @FXML private ImageView poster1Snip;
+  @FXML private ImageView vaseSnip;
+  @FXML private ImageView painting1Snip;
+  @FXML private ImageView painting2Snip;
+  @FXML private ImageView painting3Snip;
+  @FXML private ImageView painting4Snip;
+  @FXML private ImageView painting5Snip;
+  @FXML private ImageView painting6Snip;
+  @FXML private ImageView couch2Snip;
+  @FXML private ImageView couch3Snip;
+  @FXML private ImageView plantSnip;
+  @FXML private ImageView tableSnip;
 
   public void initialize() {
     String[] puzzleObjects = {
@@ -26,5 +47,61 @@ public class BookPuzzleController {
     };
     int randomNumber = (int) (Math.random() * puzzleObjects.length);
     puzzleAnswer = puzzleObjects[randomNumber];
+    switch ((String) puzzleAnswer) {
+      case "couch1":
+        couch1Snip.setVisible(true);
+        break;
+      case "dinosaur":
+        dinosaurSnip.setVisible(true);
+        break;
+      case "mask":
+        maskSnip.setVisible(true);
+        break;
+      case "robe":
+        robeSnip.setVisible(true);
+        break;
+      case "poster3":
+        poster3Snip.setVisible(true);
+        break;
+      case "poster2":
+        poster2Snip.setVisible(true);
+        break;
+      case "poster1":
+        poster1Snip.setVisible(true);
+        break;
+      case "vase":
+        vaseSnip.setVisible(true);
+        break;
+      case "painting1":
+        painting1Snip.setVisible(true);
+        break;
+      case "painting2":
+        painting2Snip.setVisible(true);
+        break;
+      case "painting3":
+        painting3Snip.setVisible(true);
+        break;
+      case "painting4":
+        painting4Snip.setVisible(true);
+        break;
+      case "painting5":
+        painting5Snip.setVisible(true);
+        break;
+      case "painting6":
+        painting6Snip.setVisible(true);
+        break;
+      case "couch2":
+        couch2Snip.setVisible(true);
+        break;
+      case "couch3":
+        couch3Snip.setVisible(true);
+        break;
+      case "plant":
+        plantSnip.setVisible(true);
+        break;
+      case "table":
+        tableSnip.setVisible(true);
+        break;
+    }
   }
 }
