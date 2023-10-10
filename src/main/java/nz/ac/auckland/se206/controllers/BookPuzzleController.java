998087efc5cf6e37e0507980df28494d0ead1e68
0x20000 <zhe570@aucklanduni.ac.nz>
2023-10-10 15:22:42 +1300

    fix:no enough dig if 0

--- src/main/java/nz/ac/auckland/se206/controllers/BookPuzzleController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/BookPuzzleController.java
@@ -21,11 +21,8 @@ public class BookPuzzleController {
       randNumber = Math.random();
       puzzleCode = (int) (randNumber * 100);
     }
-    StringBuilder sb = new StringBuilder();
-    sb.append("/images/");
-    sb.append(GameState.puzzleAnswer);
-    sb.append("Snip.png");
-    image.setImage(new Image(sb.toString())); // Set the random image for puzzle
+    image.setImage(new Image(String.format("/images/%sSnip.png",
+        GameState.puzzleAnswer))); // Set the random image for puzzle
   }
 
   @FXML
