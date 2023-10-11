6fe9d1aebc68016c5426af7e4832ea4ca07de5b6
nlia656 <nlia656@aucklanduni.ac.nz>
2023-10-11 14:33:03 +1300

    feat: added scene for the bench puzzle

---
+++ src/main/java/nz/ac/auckland/se206/controllers/BenchController.java
@@ -0,0 +1,17 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.SceneManager.AppUi;
+
+public class BenchController {
+
+  @FXML private Button goBack;
+
+  @FXML
+  void onCloseBench(ActionEvent event) {
+    App.setUi(AppUi.ART_ROOM);
+  }
+}
