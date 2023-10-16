65eb1abe80ec1cbc2720e16134c0e9cc578f41e0
Andrew Kim <126522014+andrewkim219@users.noreply.github.com>
2023-10-17 00:18:59 +1300

    refactor: unused variable and import
    Removed unused variable and import for lobby controller

--- src/main/java/nz/ac/auckland/se206/controllers/LobbyRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LobbyRoomController.java
@@ -1,15 +1,12 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 /** Controller class for the lobby room scene. Extends SceneController class. */
 public class LobbyRoomController extends SceneController {
-
-  @FXML private Label newMessage;
 
   /**
    * Initializes the lobby room scene. Called when the scene is loaded to sync the time and game
