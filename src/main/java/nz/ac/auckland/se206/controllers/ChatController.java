e89f720dfe13c8345dd3c15de36f9b038cb96843
nlia656 <nlia656@aucklanduni.ac.nz>
2023-10-17 18:47:47 +1300

    fix: chat now catches the correct answer if there is an ai error

--- src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
@@ -30,22 +30,14 @@ import nz.ac.auckland.se206.speech.TextToSpeech;
  */
 public class ChatController extends SceneController {
 
-  @FXML
-  private Text hintRemains;
-  @FXML
-  private TextArea chatTextArea;
-  @FXML
-  private TextField inputText;
-  @FXML
-  private Button sendButton;
-  @FXML
-  private Button hintButton;
-  @FXML
-  private ImageView picDinoRoom;
-  @FXML
-  private ImageView picArtRoom;
-  @FXML
-  private ImageView picLobbyRoom;
+  @FXML private Text hintRemains;
+  @FXML private TextArea chatTextArea;
+  @FXML private TextField inputText;
+  @FXML private Button sendButton;
+  @FXML private Button hintButton;
+  @FXML private ImageView picDinoRoom;
+  @FXML private ImageView picArtRoom;
+  @FXML private ImageView picLobbyRoom;
   private Timeline typingAnimation;
   private int typeTime = 0;
 
@@ -59,23 +51,26 @@ public class ChatController extends SceneController {
   @FXML
   public void initialize() {
     // Create a new thread to run the GPT model
-    Task<Void> task = new Task<>() {
-      @Override
-      protected Void call() {
-        inProcess();
-        try {
-          // Run the GPT model to generate the riddle
-          runGpt(new ChatMessage("user",
-              GptPromptEngineering.getRiddleWithGivenWord(GameState.riddleAnswer)));
-        } catch (ApiProxyException e) {
-          showApiError(e);
-        }
-        Platform.runLater(() -> {
-          finishProcess();
-        });
-        return null;
-      }
-    };
+    Task<Void> task =
+        new Task<>() {
+          @Override
+          protected Void call() {
+            inProcess();
+            try {
+              // Run the GPT model to generate the riddle
+              runGpt(
+                  new ChatMessage(
+                      "user", GptPromptEngineering.getRiddleWithGivenWord(GameState.riddleAnswer)));
+            } catch (ApiProxyException e) {
+              showApiError(e);
+            }
+            Platform.runLater(
+                () -> {
+                  finishProcess();
+                });
+            return null;
+          }
+        };
 
     // Configure the hints
     if (!GameState.isUnlimitedHint && GameState.remainsHint != 0) {
@@ -83,33 +78,37 @@ public class ChatController extends SceneController {
     }
 
     // Setting the settings for chat and hints generation
-    chatCompletionRequest = new ChatCompletionRequest().setN(1).setTemperature(0.3).setTopP(0.5)
-        .setMaxTokens(300);
-    hintCompletionRequest = new ChatCompletionRequest().setN(1).setTemperature(0.2).setTopP(0.5)
-        .setMaxTokens(300);
+    chatCompletionRequest =
+        new ChatCompletionRequest().setN(1).setTemperature(0.3).setTopP(0.5).setMaxTokens(300);
+    hintCompletionRequest =
+        new ChatCompletionRequest().setN(1).setTemperature(0.2).setTopP(0.5).setMaxTokens(300);
     Thread thread = new Thread(task);
     thread.setDaemon(true);
     thread.start();
 
     // Set up timer label
-    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.5), event -> {
-      lblTime.setText(GameState.timeLeft);
-      // Set the hints label
-      if (GameState.isHard) {
-        lblHints.setText("");
-        hintsLeft.setText("No hints!");
-      } else if (GameState.isUnlimitedHint) {
-        lblHints.setText("");
-        hintsLeft.setText("Unlimited hints!");
-      } else {
-        lblHints.setText(Integer.toString(GameState.remainsHint));
-      }
-      if (GameState.remainsHint == 0) {
-        hintButton.setVisible(false);
-      } else {
-        hintButton.setVisible(true);
-      }
-    }));
+    Timeline timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.seconds(0.5),
+                event -> {
+                  lblTime.setText(GameState.timeLeft);
+                  // Set the hints label
+                  if (GameState.isHard) {
+                    lblHints.setText("");
+                    hintsLeft.setText("No hints!");
+                  } else if (GameState.isUnlimitedHint) {
+                    lblHints.setText("");
+                    hintsLeft.setText("Unlimited hints!");
+                  } else {
+                    lblHints.setText(Integer.toString(GameState.remainsHint));
+                  }
+                  if (GameState.remainsHint == 0) {
+                    hintButton.setVisible(false);
+                  } else {
+                    hintButton.setVisible(true);
+                  }
+                }));
     timeline.setCycleCount(Timeline.INDEFINITE);
     timeline.play();
   }
@@ -117,7 +116,7 @@ public class ChatController extends SceneController {
   /**
    * Appends a chat message to the chat text area.
    *
-   * @param msg  the chat message to append
+   * @param msg the chat message to append
    * @param role the role of the sender
    */
   private void appendChatMessage(ChatMessage msg, String role) {
@@ -145,21 +144,23 @@ public class ChatController extends SceneController {
       GameState.lastMsg = result.getChatMessage().getContent();
       // Check if the riddle is resolved
       if (result.getChatMessage().getRole().equals("assistant")
-          && (GameState.lastMsg.contains("Correct") || GameState.lastMsg.contains("correct"))) {
+          && (GameState.lastMsg.contains("Correct")
+              || GameState.lastMsg.contains("correct")
+              || GameState.lastMsg.contains("Well done"))) {
         GameState.isRiddleResolved = true;
         System.out.println("MEOW");
       }
-      Task<Void> tts = new Task<>() {
-        @Override
-        protected Void call() {
-          // Run the text to speech
-          TextToSpeech tts = new TextToSpeech();
-          tts.speak(result.getChatMessage().getContent());
-          Platform.runLater(() -> {
-          });
-          return null;
-        }
-      };
+      Task<Void> tts =
+          new Task<>() {
+            @Override
+            protected Void call() {
+              // Run the text to speech
+              TextToSpeech tts = new TextToSpeech();
+              tts.speak(result.getChatMessage().getContent());
+              Platform.runLater(() -> {});
+              return null;
+            }
+          };
       if (GameState.isTts) {
         Thread thread = new Thread(tts);
         thread.setDaemon(true);
@@ -170,9 +171,7 @@ public class ChatController extends SceneController {
     }
   }
 
-  /**
-   * Sends a message to the GPT model.
-   */
+  /** Sends a message to the GPT model. */
   @FXML
   private void onSendMessage() {
     // Get the message from the text field
@@ -183,26 +182,26 @@ public class ChatController extends SceneController {
     inputText.clear();
     ChatMessage msg = new ChatMessage("user", message);
     appendChatMessage(msg, "You");
-    Task<Void> task = new Task<>() {
-      @Override
-      protected Void call() throws Exception {
-        inProcess();
-        // Run the GPT
-        runGpt(msg);
-        Platform.runLater(() -> {
-          finishProcess();
-        });
-        return null;
-      }
-    };
+    Task<Void> task =
+        new Task<>() {
+          @Override
+          protected Void call() throws Exception {
+            inProcess();
+            // Run the GPT
+            runGpt(msg);
+            Platform.runLater(
+                () -> {
+                  finishProcess();
+                });
+            return null;
+          }
+        };
     Thread thread = new Thread(task);
     thread.setDaemon(true);
     thread.start();
   }
 
-  /**
-   * Navigates back to the previous view.
-   */
+  /** Navigates back to the previous view. */
   @FXML
   private void onGoBack() {
     // Go back to previous scene
@@ -215,13 +214,15 @@ public class ChatController extends SceneController {
     }
   }
 
-  /**
-   * Displays typing animation while waiting for GPT response.
-   */
+  /** Displays typing animation while waiting for GPT response. */
   private void inProcess() {
-    typingAnimation = new Timeline(new KeyFrame(Duration.millis(300), event -> {
-      setTypingText();
-    }));
+    typingAnimation =
+        new Timeline(
+            new KeyFrame(
+                Duration.millis(300),
+                event -> {
+                  setTypingText();
+                }));
     typingAnimation.setCycleCount(Timeline.INDEFINITE); // Repeat indefinitely
     typingAnimation.play();
     inputText.setDisable(true);
@@ -237,7 +238,6 @@ public class ChatController extends SceneController {
     hintButton.setDisable(false);
   }
 
-
   /**
    * Displays an error alert for API proxy issues.
    *
@@ -252,33 +252,33 @@ public class ChatController extends SceneController {
     alert.showAndWait();
   }
 
-  /**
-   * Asks for hints from the GPT model.
-   */
+  /** Asks for hints from the GPT model. */
   @FXML
   private void onAskHint() {
-    Task<Void> task = new Task<>() {
-      @Override
-      protected Void call() {
-        // Run the GPT model to generate hints
-        inProcess();
-        try {
-          hintCompletionRequest.addMessage(
-              new ChatMessage("user", GptPromptEngineering.getHints()));
-          ChatCompletionResult hintCompletionResult = hintCompletionRequest.execute();
-          Choice result = hintCompletionResult.getChoices().iterator().next();
-          hintCompletionRequest.addMessage(result.getChatMessage());
-          appendChatMessage(result.getChatMessage(), "Security");
-          GameState.lastMsg = result.getChatMessage().getContent();
-        } catch (ApiProxyException e) {
-          showApiError(e);
-        }
-        Platform.runLater(() -> {
-          finishProcess();
-        });
-        return null;
-      }
-    };
+    Task<Void> task =
+        new Task<>() {
+          @Override
+          protected Void call() {
+            // Run the GPT model to generate hints
+            inProcess();
+            try {
+              hintCompletionRequest.addMessage(
+                  new ChatMessage("user", GptPromptEngineering.getHints()));
+              ChatCompletionResult hintCompletionResult = hintCompletionRequest.execute();
+              Choice result = hintCompletionResult.getChoices().iterator().next();
+              hintCompletionRequest.addMessage(result.getChatMessage());
+              appendChatMessage(result.getChatMessage(), "Security");
+              GameState.lastMsg = result.getChatMessage().getContent();
+            } catch (ApiProxyException e) {
+              showApiError(e);
+            }
+            Platform.runLater(
+                () -> {
+                  finishProcess();
+                });
+            return null;
+          }
+        };
     // Decrease the number of hints left
     if (!GameState.isUnlimitedHint) {
       GameState.remainsHint--;
@@ -292,9 +292,7 @@ public class ChatController extends SceneController {
     thread.start();
   }
 
-  /**
-   * Sets the chat background based on the current room.
-   */
+  /** Sets the chat background based on the current room. */
   public void setChatBackground() {
     // Set the chat background based on the current room
     if (GameState.onArtRoom) {
